28f274e987e46019616e6aa4b60b4e4b5342ec1e
David Rodenas Pico <david.rodenas@gmail.com>
2016-08-29 08:33:04 +0200

    better presentation

--- src/app/posts/components/post-prevnext.component.ts
+++ src/app/posts/components/post-prevnext.component.ts
@@ -6,11 +6,19 @@ export class PostPrevNextComponent {
   };
   static template = `
     <div class="layout-row">
-      <a ng-if=" $ctrl.post.prev" class="rel-link" href="#!/posts/{{$ctrl.post.prev.basename}}">← {{$ctrl.post.prev.title}}</a> 
-      <a ng-if="!$ctrl.post.prev" class="rel-link" href="#!/"><i class="fa fa-home"></i> HOME</a> 
-      <div class="flex"></div>
-      <a ng-if=" $ctrl.post.next" class="rel-link" href="#!/posts/{{$ctrl.post.next.basename}}">{{$ctrl.post.next.title}} →</a> 
-      <a ng-if="!$ctrl.post.next" class="rel-link" href="#!/"><i class="fa fa-home"></i> HOME</a> 
+      <a ng-if=" $ctrl.post.prev" href="#!/posts/{{$ctrl.post.prev.basename}}"
+        class="rel-link layout-row flex">
+          <span>←&nbsp;</span>
+          <span class="flex">{{$ctrl.post.prev.title}}</span>
+      </a> 
+      <a ng-if="!$ctrl.post.prev" class="rel-link flex" href="#!/"><i class="fa fa-home"></i> HOME</a> 
+
+      <a ng-if=" $ctrl.post.next" href="#!/posts/{{$ctrl.post.next.basename}}"
+        class="rel-link layout-row flex">
+          <span class="flex justify-right">{{$ctrl.post.next.title}}</span>
+          <span>&nbsp;→</span>
+      </a> 
+      <a ng-if="!$ctrl.post.next" class="rel-link flex" href="#!/"><i class="fa fa-home"></i> HOME</a> 
     </div>
   `;
   static controller = PostPrevNextComponent;
