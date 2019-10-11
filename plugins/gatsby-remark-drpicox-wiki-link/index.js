002b58213b59edad525b8c81b515d6912563af7d
David Rodenas <david.rodenas@gmail.com>
2019-10-11 12:57:33 +0200

    mdx wiki links are <Link> + remove old stuff

--- plugins/gatsby-remark-drpicox-wiki-link/index.js
+++ plugins/gatsby-remark-drpicox-wiki-link/index.js
@@ -2,6 +2,20 @@ const visit = require(`unist-util-visit`)
 
 const IS_WIKI = /^([A-Z][\w\d]*)+$/
 
+function transformExistingTopicNode(node) {
+  const { label } = node
+  node.type = "jsx"
+  node.value = `<Link to="/${label}">[${label}]</Link>`
+  delete node.children
+}
+
+function transformNewTopicNode(node) {
+  const { label } = node
+  node.type = "link"
+  node.children[0].value = `[${label}]`
+  node.url = `https://github.com/drpicox/drpicox.github.io/new/master?filename=_entries/${label}.md`
+}
+
 module.exports = ({ markdownAST, makrdownNode, files }) => {
   const existingTopics = files
     .filter(f => f.sourceInstanceName === "wiki")
@@ -10,13 +24,10 @@ module.exports = ({ markdownAST, makrdownNode, files }) => {
   visit(markdownAST, "linkReference", node => {
     const { label } = node
     if (IS_WIKI.test(label)) {
-      node.type = "link"
-      node.children[0].value = `[${label}]`
-
       if (existingTopics.includes(label)) {
-        node.url = `./${label}`
+        transformExistingTopicNode(node)
       } else {
-        node.url = `https://github.com/drpicox/drpicox.github.io/new/master?filename=_entries/${label}.md`
+        transformNewTopicNode(node)
       }
     }
   })
--- src/layouts/WikiLayout.js
+++ src/layouts/WikiLayout.js
@@ -1,17 +1,23 @@
 import React from "react"
+import { Link } from "gatsby"
+import { MDXProvider } from "@mdx-js/react"
 import Container from "../components/Container"
 import Main from "../components/Main"
 import BasicLayout from "./BasicLayout"
 
+const components = { Link }
+
 export default function WikiPage({ children, pageContext }) {
   return (
     <BasicLayout>
-      <Main>
-        <Container>
-          <h1>{pageContext.frontmatter.title}</h1>
-          {children}
-        </Container>
-      </Main>
+      <MDXProvider components={components}>
+        <Main>
+          <Container>
+            <h1>{pageContext.frontmatter.title}</h1>
+            {children}
+          </Container>
+        </Main>
+      </MDXProvider>
     </BasicLayout>
   )
 }
