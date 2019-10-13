33b39ad2ba1c8ddd15c50ebfaf3f76ee12d36795
David Rodenas <david.rodenas@gmail.com>
2019-10-14 00:47:29 +0200

    fix build redirections

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { Link } from "gatsby"
-import BasicLayout from "../layouts/BasicLayout"
-
-export default () => (
-  <BasicLayout>
-    Go <Link to="/Home">Home</Link>.
-  </BasicLayout>
-)
