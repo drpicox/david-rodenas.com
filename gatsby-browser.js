f04a520ec02f45e07f19eda86e6ec968af96dbcb
David Rodenas <david.rodenas@gmail.com>
2019-11-02 10:09:00 +0100

    add tcls2 release strategy

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,6 @@
 import "prismjs/themes/prism-dark.css"
+// import "prismjs/themes/prism-solarizedlight.css"
+// import "prism-themes/themes/prism-hopscotch.css"
 import "./src/styles.css"
 import mermaid from "mermaid"
 
@@ -6,6 +8,7 @@ global.mermaid = mermaid
 mermaid.mermaidAPI.initialize({
   startOnLoad: false,
   logLevel: 5,
+  theme: "forest",
   // securityLevel: "loose",
   // htmlLabels: false,
 })
