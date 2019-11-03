8ca661dcaff20a2c9e22e53f0f8d7a9eea13fa40
David Rodenas <david.rodenas@gmail.com>
2019-11-03 18:59:11 +0100

    fit mermaid graphs on size

--- src/shared/Mermaid.js
+++ src/shared/Mermaid.js
@@ -1,8 +1,14 @@
 import React, { useMemo } from "react"
+import { css } from "@emotion/core"
 
 const mermaid = global.mermaid || { render: () => {} }
 
 let num = 1
+const mermaidCss = css`
+  & > svg {
+    max-width: 100%;
+  }
+`
 
 export default function Mermaid({ name, children } = { name: "diagram" }) {
   const html = useMemo(
@@ -11,6 +17,10 @@ export default function Mermaid({ name, children } = { name: "diagram" }) {
   )
 
   return (
-    <div className="mermaid" dangerouslySetInnerHTML={{ __html: html }}></div>
+    <div
+      css={mermaidCss}
+      className="mermaid"
+      dangerouslySetInnerHTML={{ __html: html }}
+    ></div>
   )
 }
