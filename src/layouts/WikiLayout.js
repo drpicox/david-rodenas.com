818c664e7ef86562cb518c040a650e86aad6fa0d
David Rodenas <david.rodenas@gmail.com>
2020-03-08 17:28:26 +0100

    style: improve styles

--- src/layouts/WikiLayout.js
+++ src/layouts/WikiLayout.js
@@ -5,12 +5,13 @@ import * as shared from "../shared"
 import BasicLayout from "./BasicLayout"
 
 export default function WikiPage({ children, pageContext }) {
+  const { title } = pageContext.frontmatter
   return (
-    <BasicLayout title={pageContext.frontmatter.title}>
+    <BasicLayout title={title}>
       <MDXProvider components={shared}>
         <Main>
           <shared.Container>
-            <h1>{pageContext.frontmatter.title}</h1>
+            {title && <h1>{title}</h1>}
             {children}
           </shared.Container>
         </Main>
