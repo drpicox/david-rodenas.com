4282890280de478cb52640d9f0b11c6abd44e6b0
David Rodenas <david.rodenas+gt@gmail.com>
2025-05-03 21:10:05 +0200

    minor code style updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,9 +13,9 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off"
-    }
-  }
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '',
+  output: "export",
+  basePath: "",
   images: {
     unoptimized: true,
   },
